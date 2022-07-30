b1a3642b54cf4ac77557393c7715cc444df4eec2
Batuhan Yiğit <70327837+batuhanyigitt@users.noreply.github.com>
2022-07-30 22:51:05 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import { GraphQLClient, gql } from "graphql-request";
+import BlogCard from "../components/BlogCard";
+
+const graphcms = new GraphQLClient("https://api-eu-central-1.hygraph.com/v2/cl67unkmqcppi01t3g1c46vac/master");
+
+const QUERY = gql`
+{
+  posts {
+    id
+    title
+    datePublished
+    slug
+    content {
+      html
+    }
+    author {
+      name
+      avatar {
+        url
+      }
+    }
+    coverPhoto {
+      publishedAt
+      createdBy {
+        id
+      }
+      url
+    }
+  }
+}
+`;
+
+export async function getStaticProps() {
+const { posts } = await graphcms.request(QUERY);
+return {
+  props: {
+    posts,
+  },
+  revalidate: 30,
+};
+}
+
+export default function Home({ posts }) {
+return (
+  <div className={styles.container}>
+    <Head>
+      <title>Digital Scribbles</title>
+      <meta name="description" content="A blog tutorial made with JAMstack" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main className={styles.main}>
+      {posts.map((post) => (
+        <BlogCard
+          title={post.title}
+          author={post.author}
+          coverPhoto={post.coverPhoto}
+          key={post.id}
+          datePublished={post.datePublished}
+          slug={post.slug}
+        />
+      ))}
+    </main>
+  </div>
+);
+}
---
+++ pages/posts/[slugs].js
@@ -0,0 +1,90 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { GraphQLClient, gql } from "graphql-request";
+import styles from "../../styles/Slug.module.css";
+import moment from "moment";
+
+const graphcms = new GraphQLClient("https://api-eu-central-1.hygraph.com/v2/cl67unkmqcppi01t3g1c46vac/master");
+
+const QUERY = gql`
+  query Post($slug: String!) {
+    post(where: { slug: $slug }) {
+      id
+      title
+      slug
+      datePublished
+      author {
+        id
+        name
+        avatar {
+          url
+        }
+      }
+      content {
+        html
+      }
+      coverPhoto {
+        id
+        url
+      }
+    }
+  }
+`;
+const SLUGLIST = gql`
+  {
+    posts {
+      slug
+    }
+  }
+`;
+
+export async function getStaticPaths() {
+  const { posts } = await graphcms.request(SLUGLIST);
+  return {
+    paths: posts.map((post) => ({ params: { slug: post.slug } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+  const data = await graphcms.request(QUERY, { slug });
+  const post = data.post;
+  return {
+    props: {
+      post,
+    },
+    revalidate: 30,
+  };
+}
+
+export default function BlogPost({ post }) {
+  return (
+    <main className={styles.blog}>
+      <img
+        className={styles.cover}
+        src={post.coverPhoto.url}
+        alt={post.title}
+      />
+      <div className={styles.title}>
+        // eslint-disable-next-line react/jsx-no-comment-textnodes
+        <div className={styles.authdetails}>
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={post.author.avatar.url} alt={post.author.name} />
+          <div className={styles.authtext}>
+            <h6>By {post.author.name} </h6>
+            <h6 className={styles.date}>
+              {moment(post.datePublished).format("MMMM d, YYYY")}
+            </h6>
+          </div>
+        </div>
+        <h2>{post.title}</h2>
+      </div>
+
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: post.content.html }}
+      ></div>
+    </main>
+  );
+}
